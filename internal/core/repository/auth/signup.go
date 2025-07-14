9bd8e70a96ead35ff712c66acbaf131373fda0f5
Rafael Tomelin <carlos.tomelin@stone.com.br>
2025-07-14 15:27:26 -0300

    chore: update main

--- internal/core/repository/auth/signup.go
+++ internal/core/repository/auth/signup.go
@@ -63,6 +63,7 @@ func (r *SignupEvent) Get(ctx context.Context, filters database.Conditional) (*e
 
 func (r *SignupEvent) List(ctx context.Context, filter database.Conditional) ([]entity.Signup, error) {
 
+	fmt.Println("List SignupEvent on repository - start")
 	if filter.Field == "" {
 		return nil, errors.New("invalid filters: no field provided")
 	}
