6354ddb66632eb2e1985d4bd2a9ead2c009a6747
Rafael Tomelin <carlos.tomelin@stone.com.br>
2025-07-14 22:48:13 -0300

    chore: update main

--- internal/core/service/tenant/tenant.go
+++ internal/core/service/tenant/tenant.go
@@ -162,15 +162,12 @@ func (s *tenantEventService) Create(ctx context.Context, tenant *entity.Tenant) 
 	}
 
 	vaultid := utils.GenerateTenant()
+
 	s.authz.SetupVault(ctx, vaultid, tenant.Tenant.TenantID, tenant.Owner.Uid)
-	au, err := s.authz.GetAuditLogs(ctx, tenant.Owner.GetUid(), vaultid, tenant.Tenant.Plan.GetAuditLogLimit())
-	if err != nil {
-		fmt.Println("Failed to get audit logs:", err)
-	}
-	fmt.Println("Audit logs:", au)
 	s.authz.CreateGroup(ctx, vaultid, tenant.Tenant.TenantID, tenant.Owner.GetEmail())
 	s.authz.AddUserToGroup(ctx, tenant.Owner.GetEmail(), vaultid, authorization.GroupRoleOwner)
 
+	tenant.ID = tenant.Tenant.TenantID
 	result, err := s.repo.Create(ctx, tenant)
 	if err != nil {
 		// Salvar o erro original antes de tentar rollback
@@ -189,6 +186,7 @@ func (s *tenantEventService) Create(ctx context.Context, tenant *entity.Tenant) 
 		return nil, corev1.ErrGenericError("Failed to create tenant event")
 	}
 
+	fmt.Print("Tenant created successfully: ", result.ID)
 	return result, nil
 }
 
