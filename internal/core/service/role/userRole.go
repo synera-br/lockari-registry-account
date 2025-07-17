ff1e3d25126a826b3ea9694a2517fa2f12acb553
Rafael Tomelin <carlos.tomelin@stone.com.br>
2025-07-17 18:11:36 -0300

    chore: add userRole

--- internal/core/service/role/userRole.go
+++ internal/core/service/role/userRole.go
@@ -306,7 +306,7 @@ func (s *userRoleService) validateToken(ctx context.Context) (authenticator.User
 	fmt.Println("\n [validateToken] Getting custom claims:", userClaim.CustomClaims)
 
 	fmt.Println("\n [validateToken] Getting user claim for tenant:", userClaim.TenantID)
-	if userClaim.TenantID == "" {
+	if userClaim.CustomClaims["tenant_id"] == "" {
 		return nil, corev1.ErrGenericError("user claim tenant ID is empty")
 	}
 
