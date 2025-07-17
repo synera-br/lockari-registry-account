9c3b3e648b648dd72202e9e933885875a48a211b
Rafael Tomelin <carlos.tomelin@stone.com.br>
2025-07-17 17:33:15 -0300

    chore: add userRole

--- internal/core/service/tenant/tenant.go
+++ internal/core/service/tenant/tenant.go
@@ -237,6 +237,8 @@ func (s *tenantEventService) Get(ctx context.Context, filters entity.TenantFilte
 		return nil, errors.New("claims cannot be nil")
 	}
 
+	fmt.Println("\nGetting claims:", claims)
+	fmt.Println("\nGetting Custom claims:", claims.CustomClaims)
 	tenantID, ok := claims.CustomClaims["tenant_id"].(string)
 	if !ok {
 		return nil, fmt.Errorf("tenant_id not found in claims")
